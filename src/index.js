203e3061787a570a3c0d46a2832907527a8a3801
TatyyanaS <taty.yana.kz@gmail.com>
2023-10-20 19:07:46 +0600

    finally done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        let letter = expr.slice(i, i + 10);
+        if (letter === '**********') {
+            result += ' ';
+        } else {
+            let morseLetter = '';
+            for (let j = 0; j < letter.length; j += 2) {
+                let symbol = letter.slice(j, j + 2);
+                if (symbol === '10') {
+                    morseLetter += '.';
+                } else if (symbol === '11') {
+                    morseLetter += '-';
+                }
+            }
+            result += MORSE_TABLE[morseLetter];
+        }
+    }
+    return result;
 }
 
 module.exports = {
